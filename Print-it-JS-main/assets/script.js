1d76f73e80eb8fd2c8928b7952873bc3bf9d9ee1
LucasR <149654268+Karmouk@users.noreply.github.com>
2024-05-15 09:48:18 +0200

    Print-it Initialised

---
+++ Print-it-JS-main/assets/script.js
@@ -0,0 +1,33 @@
+
+
+const slides = [
+	{
+		"image":"slide1.jpg",
+		"tagLine":"Impressions tous formats <span>en boutique et en ligne</span>"
+	},
+	{
+		"image":"slide2.jpg",
+		"tagLine":"Tirages haute définition grand format <span>pour vos bureaux et events</span>"
+	},
+	{
+		"image":"slide3.jpg",
+		"tagLine":"Grand choix de couleurs <span>de CMJN aux pantones</span>"
+	},
+	{
+		"image":"slide4.png",
+		"tagLine":"Autocollants <span>avec découpe laser sur mesure</span>"
+	}
+]
+function slidegauche(){
+	slides_check--
+	
+}
+function slidedroite(){
+	slides_check++
+}
+let slides_check=0
+const flechedegauche=document.querySelector("#banner .arrow_left");
+flechedegauche.addEventListener("click",slidegauche);
+
+const flechededroite=document.querySelector("#banner .arrow_right");
+flechededroite.addEventListener("click",slidedroite);
